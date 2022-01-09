ece726f7a8ffc17fe55fb6ab8894eda13d46b972
Zuzanna Rec <zuzanna.rec@gmail.com>
2022-01-09 20:52:49 +0100

    init tests

---
+++ account/client_test.go
@@ -0,0 +1,26 @@
+package account
+
+import (
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+	testCases := []struct {
+		desc string
+	}{
+		{
+			desc: "",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+		})
+	}
+}
+
+func TestCreateClient(t *testing.T) {
+	_, err := NewClient()
+	if err != nil {
+		t.Fatalf("Could not create client")
+	}
+}
